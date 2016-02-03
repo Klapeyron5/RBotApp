9bdae182554c8c5c5ed5cfcc675ca43597d2cbf3
Klapeyron <Nikitoslav-razdol@ya.ru>
2016-02-03 17:00:46 +0300

    Common thread for low level navi command done
    Common thread for low level navi command done

--- java/space/klapeyron/rbotapp/LowLevelNavigationMethods.java
+++ java/space/klapeyron/rbotapp/LowLevelNavigationMethods.java
@@ -3,7 +3,10 @@ package space.klapeyron.rbotapp;
 import android.util.Log;
 
 import ru.rbot.android.bridge.service.robotcontroll.controllers.BodyController;
+import ru.rbot.android.bridge.service.robotcontroll.controllers.NeckController;
 import ru.rbot.android.bridge.service.robotcontroll.controllers.body.TwoWheelsBodyController;
+import ru.rbot.android.bridge.service.robotcontroll.controllers.neck.data.Neck;
+import ru.rbot.android.bridge.service.robotcontroll.controllers.neck.data.NeckSegment;
 import ru.rbot.android.bridge.service.robotcontroll.exceptions.ControllerException;
 import ru.rbot.android.bridge.service.robotcontroll.robots.Robot;
 
@@ -13,6 +16,10 @@ public class LowLevelNavigationMethods {
     public static final String FORWARD_MOVE = "FORWARD_MOVE";
     public static final String STOP_MOVE = "STOP_MOVE";
     public static final String BACK_MOVE = "BACK_MOVE";
+    public static final String TURN_LEFT = "TURN_LEFT";
+    public static final String TURN_RIGHT = "TURN_RIGHT";
+    public static final String NECK_UP = "NECK_UP";
+    public static final String NECK_DOWN = "NECK_DOWN";
 
     LowLevelNavigationMethods(Robot r) {
         robot = r;
@@ -30,6 +37,18 @@ public class LowLevelNavigationMethods {
                     break;
                 case BACK_MOVE:
                     backMove();
+                    break;
+                case TURN_LEFT:
+                    turnLeft();
+                    break;
+                case TURN_RIGHT:
+                    turnRight();
+                    break;
+                case NECK_UP:
+                    neckUp();
+                    break;
+                case NECK_DOWN:
+                    neckDown();
                     break;
             }
         } catch (ControllerException e) {
@@ -70,6 +89,84 @@ public class LowLevelNavigationMethods {
                 TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
                 wheelsController.setWheelsSpeeds(-20f,-20f);
             }
+        }
+    }
+
+    private void turnLeft() throws ControllerException {
+        if( robot.isControllerAvailable( BodyController.class ) )
+        {
+            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
+            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
+            {
+                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
+                wheelsController.setWheelsSpeeds(-20f,20f);
+            }
+        }
+    }
+
+    private void turnRight() throws ControllerException {
+        if( robot.isControllerAvailable( BodyController.class ) )
+        {
+            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
+            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
+            {
+                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
+                wheelsController.setWheelsSpeeds(20f,-20f);
+            }
+        }
+    }
+
+    private void neckUp() {
+        try {
+            NeckController neckController = (NeckController) robot.getController(NeckController.class);
+            Neck neck = neckController.getNeck();
+            int neckSegmentsCount = neck.getSegmentsCount();
+            NeckSegment neckSegment = neck.getNeckSegment(0);
+            neckSegment.setFlag((byte) 0x02);
+            neckSegment.setSpeed(5);
+            neckSegment.setAngle(0.5f);
+
+            neckSegment = neck.getNeckSegment(1);
+            neckSegment.setFlag((byte) 0x02);
+            neckSegment.setSpeed(5);
+            neckSegment.setAngle(0.5f);
+
+            neckSegment = neck.getNeckSegment(2);
+            neckSegment.setFlag((byte) 0x02);
+            neckSegment.setSpeed(5);
+            neckSegment.setAngle(0.5f);
+
+            //    neckSegment.move();
+            neckController.refreshNeckPosition();
+        } catch (ControllerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void neckDown() {
+        try {
+            NeckController neckController = (NeckController) robot.getController(NeckController.class);
+            Neck neck = neckController.getNeck();
+            int neckSegmentsCount = neck.getSegmentsCount();
+            NeckSegment neckSegment = neck.getNeckSegment(0);
+            neckSegment.setFlag((byte) 0x02);
+            neckSegment.setSpeed(5);
+            neckSegment.setAngle(1f);
+
+            neckSegment = neck.getNeckSegment(1);
+            neckSegment.setFlag((byte) 0x02);
+            neckSegment.setSpeed(5);
+            neckSegment.setAngle(0.0f);
+
+            neckSegment = neck.getNeckSegment(2);
+            neckSegment.setFlag((byte) 0x02);
+            neckSegment.setSpeed(5);
+            neckSegment.setAngle(1f);
+
+            //    neckSegment.move();
+            neckController.refreshNeckPosition();
+        } catch (ControllerException e) {
+            e.printStackTrace();
         }
     }
 }
--- java/space/klapeyron/rbotapp/MainActivity.java
+++ java/space/klapeyron/rbotapp/MainActivity.java
@@ -7,15 +7,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-import ru.rbot.android.bridge.service.robotcontroll.controllers.BodyController;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.NeckController;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.body.TwoWheelsBodyController;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.body.data.TwoWheelState;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.body.listeners.TwoWheelBodyControllerStateListener;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.neck.data.Neck;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.neck.data.NeckSegment;
-import ru.rbot.android.bridge.service.robotcontroll.controllers.system.listeners.Listener;
-import ru.rbot.android.bridge.service.robotcontroll.exceptions.ControllerException;
 import ru.rbot.android.bridge.service.robotcontroll.robots.Robot;
 import ru.rbot.android.bridge.service.robotcontroll.robots.listeners.RobotStateListener;
 
@@ -34,29 +25,23 @@ public class MainActivity extends Activity {
 
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnTouchListener(new View.OnTouchListener() {
-            MyThread1 thread;
+            ThreadForSimpleNavigationButtons thread;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.i(TAG, "button3 ACTION_DOWN");
-                    thread = new MyThread1();
+                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.FORWARD_MOVE);
                     thread.setRunning(true);
                     thread.start();
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Log.i(TAG, "button3 ACTION_UP");
                     boolean retry = true;
                     while (retry) {
                         thread.setRunning(false);
                         thread.interrupt();
                         retry = false;
                     }
-                    try {
-                        stopMove();
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    }
+                    lowLevelNavigationMethods.runOnKey(LowLevelNavigationMethods.STOP_MOVE);
                 }
                 return false;
             }
@@ -64,29 +49,23 @@ public class MainActivity extends Activity {
 
         Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnTouchListener(new View.OnTouchListener() {
-            MyThread2 thread;
+            ThreadForSimpleNavigationButtons thread;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.i(TAG, "button3 ACTION_DOWN");
-                    thread = new MyThread2();
+                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.BACK_MOVE);
                     thread.setRunning(true);
                     thread.start();
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Log.i(TAG, "button3 ACTION_UP");
                     boolean retry = true;
                     while (retry) {
                         thread.setRunning(false);
                         thread.interrupt();
                         retry = false;
                     }
-                    try {
-                        stopMove();
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    }
+                    lowLevelNavigationMethods.runOnKey(LowLevelNavigationMethods.STOP_MOVE);
                 }
                 return false;
             }
@@ -95,18 +74,16 @@ public class MainActivity extends Activity {
 
         Button button3 = (Button) findViewById(R.id.button3);
         button3.setOnTouchListener(new View.OnTouchListener() {
-            MyThread3 thread;
+            ThreadForSimpleNavigationButtons thread;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.i(TAG, "button3 ACTION_DOWN");
-                    thread = new MyThread3();
+                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.TURN_LEFT);
                     thread.setRunning(true);
                     thread.start();
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Log.i(TAG, "button3 ACTION_UP");
                     boolean retry = true;
                     while (retry) {
                         thread.setRunning(false);
@@ -118,11 +95,7 @@ public class MainActivity extends Activity {
                         }
                         retry = false;
                     }
-                    try {
-                        stopMove();
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    }
+                    lowLevelNavigationMethods.runOnKey(LowLevelNavigationMethods.STOP_MOVE);
                 }
                 return false;
             }
@@ -131,12 +104,12 @@ public class MainActivity extends Activity {
 
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnTouchListener(new View.OnTouchListener() {
-            MyThread4 thread;
+            ThreadForSimpleNavigationButtons thread;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    thread = new MyThread4();
+                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.TURN_RIGHT);
                     thread.setRunning(true);
                     thread.start();
                 }
@@ -152,11 +125,7 @@ public class MainActivity extends Activity {
                         }
                         retry = false;
                     }
-                    try {
-                        stopMove();
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    }
+                    lowLevelNavigationMethods.runOnKey(LowLevelNavigationMethods.STOP_MOVE);
                 }
                 return false;
             }
@@ -165,12 +134,12 @@ public class MainActivity extends Activity {
 
         Button button5 = (Button) findViewById(R.id.button5);
         button5.setOnTouchListener(new View.OnTouchListener() {
-            MyThread5 thread;
+            ThreadForSimpleNavigationButtons thread;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    thread = new MyThread5();
+                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.NECK_UP);
                     thread.setRunning(true);
                     thread.start();
                 }
@@ -193,12 +162,12 @@ public class MainActivity extends Activity {
 
         Button button6 = (Button) findViewById(R.id.button6);
         button6.setOnTouchListener(new View.OnTouchListener() {
-            MyThread6 thread;
+            ThreadForSimpleNavigationButtons thread;
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    thread = new MyThread6();
+                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.NECK_DOWN);
                     thread.setRunning(true);
                     thread.start();
                 }
@@ -213,34 +182,6 @@ public class MainActivity extends Activity {
                             e.printStackTrace();
                         }
                         retry = false;
-                    }
-                }
-                return false;
-            }
-        });
-
-        Button button7 = (Button) findViewById(R.id.button7);
-        button7.setOnTouchListener(new View.OnTouchListener() {
-            ThreadForSimpleNavigationButtons thread;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    thread = new ThreadForSimpleNavigationButtons(LowLevelNavigationMethods.FORWARD_MOVE);
-                    thread.setRunning(true);
-                    thread.start();
-                }
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    boolean retry = true;
-                    while (retry) {
-                        thread.setRunning(false);
-                        thread.interrupt();
-                        retry = false;
-                    }
-                    try {
-                        stopMove();
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
                     }
                 }
                 return false;
@@ -273,270 +214,6 @@ public class MainActivity extends Activity {
         robot.setRobotStateListener(robotStateListener);
         robot.start();
         Log.i(TAG, "init robot finished");
-    }
-
-    private void neckUp() {
-        try {
-            NeckController neckController = (NeckController) robot.getController(NeckController.class);
-            Neck neck = neckController.getNeck();
-            int neckSegmentsCount = neck.getSegmentsCount();
-            NeckSegment neckSegment = neck.getNeckSegment(0);
-            neckSegment.setFlag((byte) 0x02);
-            neckSegment.setSpeed(5);
-            neckSegment.setAngle(0.5f);
-
-            neckSegment = neck.getNeckSegment(1);
-            neckSegment.setFlag((byte) 0x02);
-            neckSegment.setSpeed(5);
-            neckSegment.setAngle(0.5f);
-
-            neckSegment = neck.getNeckSegment(2);
-            neckSegment.setFlag((byte) 0x02);
-            neckSegment.setSpeed(5);
-            neckSegment.setAngle(0.5f);
-
-        //    neckSegment.move();
-            neckController.refreshNeckPosition();
-        } catch (ControllerException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void neckDown() {
-        try {
-            NeckController neckController = (NeckController) robot.getController(NeckController.class);
-            Neck neck = neckController.getNeck();
-            int neckSegmentsCount = neck.getSegmentsCount();
-            NeckSegment neckSegment = neck.getNeckSegment(0);
-            neckSegment.setFlag((byte) 0x02);
-            neckSegment.setSpeed(5);
-            neckSegment.setAngle(1f);
-
-            neckSegment = neck.getNeckSegment(1);
-            neckSegment.setFlag((byte) 0x02);
-            neckSegment.setSpeed(5);
-            neckSegment.setAngle(0.0f);
-
-            neckSegment = neck.getNeckSegment(2);
-            neckSegment.setFlag((byte) 0x02);
-            neckSegment.setSpeed(5);
-            neckSegment.setAngle(1f);
-
-            //    neckSegment.move();
-            neckController.refreshNeckPosition();
-        } catch (ControllerException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void forwardMove() throws ControllerException {
-        if( robot.isControllerAvailable( BodyController.class ) )
-        {
-            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
-            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
-            {
-                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
-                wheelsController.setWheelsSpeeds(20f,20f);
-            }
-        }
-    }
-
-    private void stopMove() throws ControllerException {
-        if( robot.isControllerAvailable( BodyController.class ) )
-        {
-            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
-            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
-            {
-                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
-                wheelsController.setWheelsSpeeds(0.0f,0.0f);
-            }
-        }
-        Log.i(TAG, "stopMove() finished");
-    }
-
-    private void backMove() throws ControllerException {
-        if( robot.isControllerAvailable( BodyController.class ) )
-        {
-            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
-            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
-            {
-                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
-                wheelsController.setWheelsSpeeds(-20f,-20f);
-            }
-        }
-        Log.i(TAG, "backMove() finished");
-    }
-
-    private void turnLeft() throws ControllerException {
-        if( robot.isControllerAvailable( BodyController.class ) )
-        {
-            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
-            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
-            {
-                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
-                wheelsController.setWheelsSpeeds(-20f,20f);
-            }
-        }
-    }
-
-    private void turnRight() throws ControllerException {
-        if( robot.isControllerAvailable( BodyController.class ) )
-        {
-            BodyController bodyController = (BodyController) robot.getController( BodyController.class );
-            if( bodyController.isControllerAvailable( TwoWheelsBodyController.class ) )
-            {
-                TwoWheelsBodyController wheelsController = (TwoWheelsBodyController) bodyController.getController( TwoWheelsBodyController.class );
-                wheelsController.setWheelsSpeeds(20f,-20f);
-            }
-        }
-    }
-
-
-
-    class MyThread1 extends Thread {
-        private boolean running = false;
-
-        @Override
-        public void run() {
-            while(true) {
-                if(running)
-                    try {
-                        forwardMove();
-                        sleep(600);
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-            //            e.printStackTrace();
-                    }
-                else
-                    return;
-            }
-        }
-
-        public void setRunning(boolean b) {
-            running = b;
-        }
-    }
-
-    class MyThread2 extends Thread {
-        private boolean running = false;
-
-        @Override
-        public void run() {
-            while(true) {
-                if(running)
-                    try {
-                        backMove();
-                        sleep(600);
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-         //               e.printStackTrace();
-                    }
-                else
-                    return;
-            }
-        }
-
-        public void setRunning(boolean b) {
-            running = b;
-        }
-    }
-
-    class MyThread3 extends Thread {
-        private boolean running = false;
-
-        @Override
-        public void run() {
-            while(true) {
-                if(running)
-                    try {
-                        turnLeft();
-                        sleep(600);
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        //               e.printStackTrace();
-                    }
-                else
-                    return;
-            }
-        }
-
-        public void setRunning(boolean b) {
-            running = b;
-        }
-    }
-
-    class MyThread4 extends Thread {
-        private boolean running = false;
-
-        @Override
-        public void run() {
-            while(true) {
-                if(running)
-                    try {
-                        turnRight();
-                        sleep(600);
-                    } catch (ControllerException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        //            e.printStackTrace();
-                    }
-                else
-                    return;
-            }
-        }
-
-        public void setRunning(boolean b) {
-            running = b;
-        }
-    }
-
-    class MyThread5 extends Thread {
-        private boolean running = false;
-
-        @Override
-        public void run() {
-            while(true) {
-                if(running)
-                    try {
-                        neckUp();
-                        sleep(600);
-                    } catch (InterruptedException e) {
-                        //            e.printStackTrace();
-                    }
-                else
-                    return;
-            }
-        }
-
-        public void setRunning(boolean b) {
-            running = b;
-        }
-    }
-
-    class MyThread6 extends Thread {
-        private boolean running = false;
-
-        @Override
-        public void run() {
-            while(true) {
-                if(running)
-                    try {
-                        neckDown();
-                        sleep(600);
-                    } catch (InterruptedException e) {
-                        //            e.printStackTrace();
-                    }
-                else
-                    return;
-            }
-        }
-
-        public void setRunning(boolean b) {
-            running = b;
-        }
     }
 
 

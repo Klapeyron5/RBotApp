ad938c29f228d80ddde79479d679fb8690befc35
berzin11295 <berzin@phystech.edu>
2016-02-06 17:53:19 +0300

    new class

--- java/space/klapeyron/rbotapp/Navigation.java
+++ java/space/klapeyron/rbotapp/Navigation.java
@@ -1,61 +1,138 @@
 package space.klapeyron.rbotapp;
 
-public class Navigation
-{
+public class Navigation {
     Navigation() {}
-
+    /*int[][] landscape = {{1,1,1,1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,0,0,0,0,0,0,0,0,0,1},
+			 {1,1,1,0,0,0,0,0,0,0,0,0,1},
+               		 {1,1,1,0,0,0,0,0,0,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,1,1,1,1,0,0,0,1},
+			 {1,1,1,0,0,0,0,0,0,0,0,0,1},
+			 {1,1,1,0,0,0,0,0,0,0,0,0,1},
+			 {1,1,1,0,0,0,0,0,0,0,0,0,1},
+			 {1,1,1,0,0,0,0,0,0,0,1,1,1},
+			 {1,1,1,0,0,0,0,0,0,0,1,1,1},
+			 {1,1,1,0,0,0,0,0,0,0,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1,1,1,1}};*/
     int[][] landscape = {{1,1,1,1,1},
-                         {1,0,0,0,1},
-                         {1,0,1,0,1},
-                         {1,0,0,0,1},
-                         {1,1,1,1,1}}; // Ландшафт карты. Нули обозначают разрешенные области, единицы - запрещенные.
+            {1,0,0,0,1},
+            {1,0,1,0,1},
+            {1,0,0,0,1},
+            {1,1,1,1,1}}; // Ландшафт карты. Нули обозначают разрешенные области, единицы - запрещенные.
     int m = landscape.length; //длина карты
     int n = landscape[0].length; // высота карты
 
-    int[] start = {2,2}; // Начальная точка
-    int[] finish = {4,4}; // Конечная точка
+    int[] start = {1,1}; // Начальная точка
+    int[] finish = {3,3}; // Конечная точка
     int[] tick = start; // Текущая точка (изначально равна начальной)
     int[][] neighbour = new int[4][3]; // Матрица координат (первые две строки) и значений эвристической функции (третья строка) четырех точек-соседей (столбец соответсвует номеру)
     int F; // Эвристическая функция
     int f;
-    int x,y;
-    int i,j;
+    int i,j,k;
 
     /*ArrayList consideredPoints = new ArrayList();
     ArrayList newPoints = new ArrayList(); // Попытка создать динамический массив*/
 
     int[][] consideredPoints = new int[m*n][4]; // Массив рассмотренных точек (максимально их количество может быть равно m*n).
-   // int[][] newPoints = new int[m*n][4]; // Первые две строчки - координаты рассматриваемой точки. Вторые две - Координаты "материнской" точки, на которую ссылается данная.
+    // int[][] newPoints = new int[m*n][4]; // Первые две строчки - координаты рассматриваемой точки. Вторые две - Координаты "материнской" точки, на которую ссылается данная.
 
-    private void getNeighbours() //Нахождение соседей текущей точки
+    public void getPath()
     {
-        neighbour[0][0] = tick[0]+1; neighbour[0][1] = tick[1];
-        neighbour[1][0] = tick[0]; neighbour[1][1] = tick[1]-1;
-        neighbour[2][0] = tick[0]-1; neighbour[2][1] = tick[1];
-        neighbour[3][0] = tick[0]; neighbour[3][1] = tick[1]+1;
-    }
-    private int getFunction() //Нахождение эвристической функции оптимального продолжения маршрута
-    {
-        getNeighbours();
-        F = Math.abs(neighbour[i][0]-finish[0]) + Math.abs(neighbour[i][1] - finish[1]) + (m+n+1)*landscape[tick[0]][tick[1]];
-        return F;
-    }
-    private void getPoint() //Нахождение оптимальной точки продолжения пути из текущей
-    {
-        for (i=0;i<=3;i++)
+        buildPath();
+        int Path[][] = new int[k+1][2];
+        for(i=0;i<k;i++)
         {
+            Path[i][0] = consideredPoints[i][2];
+            Path[i][1] = consideredPoints[i][3];
+        }
+        Path[k][0] = consideredPoints[k-1][0];
+        Path[k][1] = consideredPoints[k-1][1];
+
+        for(i=0;i<(k+1);i++)
+        {
+            System.out.println("("+Path[i][0]+";"+Path[i][1]+")");
+        }
+    }
+    public void buildPath()
+    {
+        /*getPoint();
+        tick[0] = 1;
+        tick[1] = 3;
+        getPoint();
+        tick[0] = 2;
+        tick[1] = 3;*/
+
+        k = 0;
+        //for (k=0;k<4;k++)
+        while(true)
+        {
+            getPoint();
+            consideredPoints[k][2] = tick[0];
+            consideredPoints[k][3] = tick[1];
+            tick[0] = neighbour[j][0];
+            tick[1] = neighbour[j][1];
+            consideredPoints[k][0] = tick[0];
+            consideredPoints[k][1] = tick[1];
+            k++;
+            if (tick[0] == finish[0])
+            {
+                if (tick[1] == finish[1])
+                {
+                    break;
+                }
+            }
+        }
+        /*while((tick[0] != finish[0]) && (tick[1] != finish[1]))
+        {
+            getPoint();
+            consideredPoints[k][2] = tick[0];
+            consideredPoints[k][3] = tick[1];
+            //System.out.println("("+consideredPoints[k][2]+";"+consideredPoints[k][3]+")");
+            tick[0] = neighbour[j][0];
+            tick[1] = neighbour[j][1];
+            //System.out.println("{"+tick[0]+";"+tick[1]+"}");
+            consideredPoints[k][0] = tick[0];
+            consideredPoints[k][1] = tick[1];
+            //System.out.println("["+consideredPoints[k][0]+";"+consideredPoints[k][1]+"]");
+            //System.out.println(j);
+            k++;
+
+            if (k > m*n)
+            {
+                break;
+                // зациклено (тупик, например)
+            }
+        }*/
+        //System.out.println(k);
+    }
+    public void getPoint() //Нахождение оптимальной точки продолжения пути из текущей
+    {
+        for (i=0;i<4;i++)
+        {
+
             neighbour[i][2] = getFunction(); // Для каждого "соседа" узнаем функцию F
+            /*System.out.println("Function(getPoint):");
+            System.out.println(neighbour[i][2]);*/
+
         }
 
         f = neighbour[0][2]; //здесь и далее находим минимальное значение f функции F
-
-        for(i=1;i<=3;i++)
+        //System.out.println("f*="+f);
+        for(i=1;i<4;i++)
         {
-            if (neighbour[i][2]<neighbour[i-1][2])
+            if (neighbour[i][2]<f)
             {
                 f = neighbour[i][2]; // Номер соседа с наименьшим значением функции.
                 j = i;
+                //System.out.println("f="+f);
             }
+            else j = 0;
         }
         for(i=3;i>j;i--)
         {
@@ -65,46 +142,36 @@ public class Navigation
                 {
                     f = neighbour[i][2]; // Таким образом, сосед под номером j имеет наименьшее значение f функции F.
                     j = i;
+                    //System.out.println("f="+f);
                 }
             }
         }
+        /*System.out.println("Minimum(F):");
+        System.out.println("j="+j);
+        System.out.println(neighbour[j][2]+"("+neighbour[j][0]+";"+neighbour[j][1]+")");*/
     }
-    public void buildPath()
+    public int getFunction() //Нахождение эвристической функции оптимального продолжения маршрута
     {
-        /*newPoints[0][0] = tick[0];
-        newPoints[0][1] = tick[1]; // Добавляем стартовую точку (равна текущей в начале) в список ожидаемых точек. */
-        i = 0;
+        getNeighbours();
+        F = Math.abs(neighbour[i][0]-finish[0]) + Math.abs(neighbour[i][1] - finish[1]) + (m+n+1)*(landscape[neighbour[i][0]][neighbour[i][1]]);
+        return F;
 
-        while((tick[0] != finish[0]) && (tick[1] != finish[1]))
+        /*System.out.println("Functions:");
+        for (i=0;i<4;i++)
         {
-            getPoint();
-            consideredPoints[i][2] = tick[0];
-            consideredPoints[i][3] = tick[1];
-            tick[0] = neighbour[j][0];
-            tick[1] = neighbour[j][1];
-            consideredPoints[i][0] = tick[0];
-            consideredPoints[i][1] = tick[1];
-
-            i = i + 1;
-
-            if (i > m*n)
-            {
-                break;
-                // зациклено (тупик, например)
-            }
-        }
+        F = Math.abs(neighbour[i][0]-finish[0]) + Math.abs(neighbour[i][1] - finish[1]) + (m+n+1)*(landscape[neighbour[i][0]][neighbour[i][1]]);
+        System.out.println(F);
+        }*/
     }
-    public int getPath() {
-    buildPath();
-
-    /*int Path[][] = new int[i][2];
-    for(j=0;j<i;j++)
+    public void getNeighbours() //Нахождение соседей текущей точки
     {
-        Path[i][0] = consideredPoints[i][2];
-        Path[i][1] = consideredPoints[i][3];
-    }
-    Path[j][0] = consideredPoints[i][0];
-    Path[j][1] = consideredPoints[i][1];*/
-        return (i+1); // Проверь эту хуйню
+        neighbour[0][0] = tick[0]+1; neighbour[0][1] = tick[1];
+        neighbour[1][0] = tick[0]; neighbour[1][1] = tick[1]+1;
+        neighbour[2][0] = tick[0]-1; neighbour[2][1] = tick[1];
+        neighbour[3][0] = tick[0]; neighbour[3][1] = tick[1]-1;
+
+        /*System.out.println("Neighbours:");
+        for (i=0;i<4;i++)
+        System.out.println(i+"("+neighbour[i][0]+";"+neighbour[i][1]+")");*/
     }
 }

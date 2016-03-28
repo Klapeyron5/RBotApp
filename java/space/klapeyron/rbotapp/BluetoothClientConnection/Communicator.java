b59cacd309ec8bef1d155add8b0ed063b1732551
Klapeyron <Nikitoslav-razdol@ya.ru>
2016-03-28 18:42:28 +0300

    Deleted old bluetooth classes

--- java/space/klapeyron/rbotapp/BluetoothClientConnection/Communicator.java
+++
@@ -1,7 +0,0 @@
-package space.klapeyron.rbotapp.BluetoothClientConnection;
-
-public interface Communicator {
-    void startCommunication();
-    void write(String message);
-    void stopCommunication();
-}
--- java/space/klapeyron/rbotapp/BluetoothClientConnection/CommunicatorService.java
+++
@@ -1,7 +0,0 @@
-package space.klapeyron.rbotapp.BluetoothClientConnection;
-
-import android.bluetooth.BluetoothSocket;
-
-public interface CommunicatorService {
-    Communicator createCommunicatorThread(BluetoothSocket socket);
-}
